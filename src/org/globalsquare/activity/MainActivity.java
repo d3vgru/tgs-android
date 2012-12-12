d3ed67d75bfdd4a5ef0575a50a497d5f7372dfd8
Ed Knutson <knutsoned@gmail.com>
2012-12-12 16:00:47 -0600

    show/hide composer, new icons

--- src/org/globalsquare/activity/MainActivity.java
+++ src/org/globalsquare/activity/MainActivity.java
@@ -1,55 +1,35 @@
 package org.globalsquare.activity;
 
-import java.util.*;
-
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.*;
-import android.support.v4.view.*;
-import android.widget.*;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.ActionBar.Tab;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.*;
 
 import org.globalsquare.*;
 import org.globalsquare.framework.*;
 
-public class MainActivity extends SherlockFragmentActivity {
+public class MainActivity extends TabbedFragmentActivity {
 	public final static int TAB_SEARCH = 0;
 	public final static int TAB_OVERVIEW = 1;
 	
+	private boolean composerShowing = false;
 	
-	ViewPager mViewPager;
-    TabsAdapter mTabsAdapter;
-    TextView tabCenter;
-    TextView tabText;
-    
-    /*
-    ListFragment mOverviewFragment;
-    
-    public void setOverviewFragment(ListFragment overviewFragment) {
-    	mOverviewFragment = overviewFragment;
-    }
-    */
-    
-    
-    // adapted from https://bitbucket.org/owentech/abstabsviewpager
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		ActionBar bar = getSupportActionBar();
-		
-        setContentView(R.layout.main_activity);
+
+		setContentView(R.layout.main_activity);
 
         setTitle(getString(R.string.short_name));
-        
-        bar.setDisplayShowHomeEnabled(false);
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-		mViewPager = (ViewPager)findViewById(R.id.pager);
+		ActionBar bar = getSupportActionBar();
+		
+		// make sure to setContentView() first
+		mViewPager = (ViewPager)findViewById(R.id.mainActivityPager);
 
 		mTabsAdapter = new TabsAdapter(this, mViewPager);
 
@@ -69,10 +49,9 @@ public class MainActivity extends SherlockFragmentActivity {
                 bar.newTab().setText("Another Square"),
                 MessageListFragment.class, null);
         
-        // select Overview tab
+        // select Overview (My Squares) tab
         mViewPager.setCurrentItem(TAB_OVERVIEW);
 	}
-	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -81,93 +60,54 @@ public class MainActivity extends SherlockFragmentActivity {
 	    return true;
 	}
 
-
-	// adapted from https://bitbucket.org/owentech/abstabsviewpager
-	public static class TabsAdapter extends FragmentPagerAdapter implements
-    ActionBar.TabListener, ViewPager.OnPageChangeListener {
-		private final Context mContext;
-		private final ActionBar mActionBar;
-		private final ViewPager mViewPager;
-		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
-
-		static final class TabInfo {
-			private final Class<?> clss;
-			private final Bundle args;
-
-			TabInfo(Class<?> _class, Bundle _args) {
-				clss = _class;
-				args = _args;
-			}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+			case R.id.menu_create:
+				Toast.makeText(this, R.string.createBtnLabel, Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.menu_compose:
+				showComposer();
+				break;
+			case R.id.menu_help:
+				Toast.makeText(this, R.string.helpBtnLabel, Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.menu_refresh:
+				Toast.makeText(this, R.string.refreshBtnLabel, Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.menu_search:
+				// select Search tab
+		        mViewPager.setCurrentItem(TAB_SEARCH);
+				break;
+			case R.id.menu_settings:
+				Toast.makeText(this, R.string.settingsBtnLabel, Toast.LENGTH_SHORT).show();
+				break;
+			default:
+				// unknown option, maybe a superclass can handle it
+				return super.onOptionsItemSelected(item);
 		}
-
-		public TabsAdapter(SherlockFragmentActivity activity, ViewPager pager) {
-			super(activity.getSupportFragmentManager());
-			mContext = activity;
-			mActionBar = activity.getSupportActionBar();
-			mViewPager = pager;
-			mViewPager.setAdapter(this);
-			mViewPager.setOnPageChangeListener(this);
-		}
-
-		public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
-			TabInfo info = new TabInfo(clss, args);
-			tab.setTag(info);
-			tab.setTabListener(this);
-			mTabs.add(info);
-			mActionBar.addTab(tab);
-			notifyDataSetChanged();
-		}
-
-		@Override
-		public int getCount() {
-			return mTabs.size();
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			TabInfo info = mTabs.get(position);
-			return Fragment.instantiate(mContext, info.clss.getName(),
-                    info.args);
-		}
-
-		public void onPageScrolled(int position, float positionOffset,
-            int positionOffsetPixels) {
-		}
-
-		public void onPageSelected(int position)
-		{
-			mActionBar.setSelectedNavigationItem(position);
-		}
-
-		public void onPageScrollStateChanged(int state) {
-		}
-
-		public void onTabSelected(Tab tab, FragmentTransaction ft) {
-			Object tag = tab.getTag();
-			for (int i = 0; i < mTabs.size(); i++) {
-				if (mTabs.get(i) == tag) {
-                    mViewPager.setCurrentItem(i);
-                    
-                    /*
-                    switch(i) {
-                		case TAB_SEARCH: // search
-                			break;
-                		case TAB_OVERVIEW: // "my squares" (combined history)
-                			break;
-                    	default:
-                    		break;
-                    }
-                    */
-                    break;
-				}
-			}
-		}
-
-		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-		}
-
-		public void onTabReselected(Tab tab, FragmentTransaction ft) {
-		}
+		return true;
 	}
 	
+	@Override
+	public void onBackPressed() {
+		if(composerShowing)
+			hideComposer();
+		else super.onBackPressed();
+	}
+	
+	private ViewGroup getComposer() {
+		return (ViewGroup)findViewById(R.id.mainActivityComposer);
+	}
+	
+	public void showComposer() {
+		getComposer().setVisibility(View.VISIBLE);
+		composerShowing = true;
+	}
+	
+	public void hideComposer() {
+		getComposer().setVisibility(View.GONE);
+		composerShowing = false;
+	}
+
 }
